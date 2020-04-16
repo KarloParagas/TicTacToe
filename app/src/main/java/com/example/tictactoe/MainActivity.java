fec0e972711db23537045de8018e3f06a406346d
KarloParagas <karloparagas@gmail.com>
2020-04-15 22:26:31 -0700

    Completed game functionality

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,12 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
     //Fields
-    private Button[] buttons;
-    private TextView turn;
+    private Button[][] buttons;
+    private TextView textMessage;
     private Player x;
     private Player o;
     public Player currentPlayer;
@@ -24,25 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         //Connect and initialize the buttons array and turn TextView to activity_main.xml
         createButtons();
-        turn = findViewById(R.id.playerTurn);
+        textMessage = findViewById(R.id.playerTurn);
         
         //Start the game
         startGame();
     }
 
     private void createButtons() {
-        //Create an array of buttons created from activity_main.xml
-        buttons = new Button[]
+        //Create a two dimensional array of buttons created from activity_main.xml
+        buttons = new Button[][]
         {
-            findViewById(R.id.button1),
-            findViewById(R.id.button2),
-            findViewById(R.id.button3),
-            findViewById(R.id.button4),
-            findViewById(R.id.button5),
-            findViewById(R.id.button6),
-            findViewById(R.id.button7),
-            findViewById(R.id.button8),
-            findViewById(R.id.button9)
+            {findViewById(R.id.button1), findViewById(R.id.button2), findViewById(R.id.button3)},
+            {findViewById(R.id.button4), findViewById(R.id.button5), findViewById(R.id.button6)},
+            {findViewById(R.id.button7), findViewById(R.id.button8), findViewById(R.id.button9)}
         };
     }
 
@@ -52,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         o = new Player("O");
 
         //Set a current player
-        currentPlayer = getPlayer();
+        currentPlayer = x;
 
         //Display that text in the "turn" field
         displayCurrentPlayer();
@@ -63,21 +55,83 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClick(View v) {
         //Loop through the button array and determine which button the user has clicked
-        for (int i = 0; i < buttons.length; i++) {
-            if (v == buttons[i] && buttons[i].getText() == "") {
-                buttons[i].setText(currentPlayer.playerName);
-                changePlayer();
+        for (int rows = 0; rows < buttons.length; rows++) {
+            for (int cols = 0; cols < buttons[0].length; cols++) {
+                if (v == buttons[rows][cols] && buttons[rows][cols].getText() == "") {
+                    buttons[rows][cols].setText(currentPlayer.playerName);
+                }
+            }
+        }
+
+        //Check the game's current state
+        if (checkWinner("X")) {
+            textDisplay("Player X Won!");
+            gameOver();
+        }
+        else if (checkWinner("O")) {
+            textDisplay("Player O Won!");
+            gameOver();
+        }
+        else if (tieGame()) {
+            textDisplay("Tie Game!");
+            gameOver();
+        }
+        else {
+            changePlayer();
+        }
+    }
+
+    private boolean tieGame() {
+        //Loop through each buttons and see if it's all full
+        for (int rows = 0; rows < buttons.length; rows++) {
+            for (int cols = 0; cols < buttons[0].length; cols++) {
+                if (buttons[rows][cols].getText() == "") {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkWinner(String player) {
+        //Horizontal combination
+        if (buttons[0][0].getText() == player && buttons[0][1].getText() == player && buttons[0][2].getText() == player ||
+            buttons[1][0].getText() == player && buttons[1][1].getText() == player && buttons[1][2].getText() == player ||
+            buttons[2][0].getText() == player && buttons[2][1].getText() == player && buttons[2][2].getText() == player) {
+            return true;
+        }
+        //Vertical combination
+        else if (buttons[0][0].getText() == player && buttons[1][0].getText() == player && buttons[2][0].getText() == player ||
+                 buttons[0][1].getText() == player && buttons[1][1].getText() == player && buttons[2][1].getText() == player ||
+                 buttons[0][2].getText() == player && buttons[1][2].getText() == player && buttons[2][2].getText() == player) {
+            return true;
+        }
+        //Diagonal combination
+        else if (buttons[0][0].getText() == player && buttons[1][1].getText() == player && buttons[2][2].getText() == player ||
+                 buttons[0][2].getText() == player && buttons[1][1].getText() == player && buttons[2][0].getText() == player) {
+            return true;
+        }
+        return false;
+    }
+
+    private void gameOver() {
+        //Disable every button
+        for (int rows = 0; rows < buttons.length; rows++) {
+            for (int cols = 0; cols < buttons[0].length; cols++) {
+                buttons[rows][cols].setEnabled(false);
             }
         }
     }
 
     public void onClickNewGame(View v) {
         //Clear all text from the buttons
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i].setText("");
+        for (int rows = 0; rows < buttons.length; rows++) {
+            for (int cols = 0; cols < buttons[0].length; cols++) {
+                buttons[rows][cols].setText("");
+                buttons[rows][cols].setEnabled(true);
+            }
         }
-
-        //Randomize a new player to start
+        //Start the game
         startGame();
     }
 
@@ -93,21 +147,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayCurrentPlayer() {
-        turn.setText("Player " + currentPlayer.playerName + "'s Turn");
+        textDisplay("Player " + currentPlayer.playerName + "'s Turn");
     }
 
-    /**
-     * This method is only executed when the application is started.
-     * It will randomize which player is the first one to start.
-     */
-    private Player getPlayer() {
-        Random rand = new Random();
-        int num = rand.nextInt(2); //Generate a number from 0 - 1
-        if (num == 0) {
-            return x;
-        }
-        else {
-            return o;
-        }
+    private void textDisplay(String s) {
+        textMessage.setText(s);
     }
 }
