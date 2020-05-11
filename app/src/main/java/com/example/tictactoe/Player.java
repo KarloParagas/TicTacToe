a00cd3b1e317a50535c02f87dc954ca8dd189dab
KarloParagas <karloparagas@gmail.com>
2020-05-11 05:34:39 -0700

    Added a boolean that keeps track if the player just played their turn

--- app/src/main/java/com/example/tictactoe/Player.java
+++ app/src/main/java/com/example/tictactoe/Player.java
@@ -2,6 +2,7 @@ package com.example.tictactoe;
 
 public class Player {
     public String playerName;
+    public boolean justPlayed = false;
 
     public Player (String name) {
         playerName = name;
